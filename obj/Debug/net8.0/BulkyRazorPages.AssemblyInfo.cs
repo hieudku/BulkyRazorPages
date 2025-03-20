b44468bf5a5aaa56fbbe8c116d31672caa815c09
Hieu <hieu.dcu@gmail.com>
2025-03-21 12:12:59 +1300

    initial commit

--- obj/Debug/net8.0/BulkyRazorPages.AssemblyInfo.cs
+++ obj/Debug/net8.0/BulkyRazorPages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyRazorPages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c023d3ee78bd762421e04afbdf70afba48a3b829")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyRazorPages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyRazorPages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
