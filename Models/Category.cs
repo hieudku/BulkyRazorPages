b8583dbe03f4da6463d4ba2b665f44b82231ec5e
Hieu <hieu.dcu@gmail.com>
2025-03-21 12:20:16 +1300

    Create Category class - Models

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyRazorPages.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Range(1, 100, ErrorMessage ="Please enter between 1 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
